dcf6fe623849731e67b812231ef49839965a66c7
muhamad fadli <2001020049@student.umrah.ac.id>
2022-11-22 10:15:20 +0700

    perbaikan fragment kalender

--- app/src/main/java/com/fadli/catcare/ui/dashboard/calendar/CalendarFragment.kt
+++ app/src/main/java/com/fadli/catcare/ui/dashboard/calendar/CalendarFragment.kt
@@ -1,10 +1,10 @@
 package com.fadli.catcare.ui.dashboard.calendar
 
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.fadli.catcare.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
