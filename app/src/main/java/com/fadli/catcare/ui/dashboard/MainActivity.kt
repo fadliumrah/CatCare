9558e7c2f1d952e3bdba008dc6096e7620b0d501
Gevano Randhi Pilko <81931731+gcoderf@users.noreply.github.com>
2022-12-13 01:39:46 +0700

    Revert "Halaman Camera Gallery"

--- app/src/main/java/com/fadli/catcare/ui/dashboard/MainActivity.kt
+++ app/src/main/java/com/fadli/catcare/ui/dashboard/MainActivity.kt
@@ -8,26 +8,28 @@ import com.fadli.catcare.ui.auth.AuthActivity
 import com.fadli.catcare.ui.dashboard.profile.ProfileFragment
 import com.fadli.catcare.R
 import com.fadli.catcare.databinding.ActivityMainBinding
-
 import com.fadli.catcare.ui.dashboard.calendar.CalendarFragment
 import com.fadli.catcare.ui.dashboard.care.CareActivity
 import com.fadli.catcare.ui.dashboard.care.CareFragment
-import com.fadli.catcare.ui.dashboard.gallery.GalleryActivity
+import com.fadli.catcare.ui.dashboard.gallery.GalleryFragment
 import com.fadli.catcare.ui.dashboard.home.HomeFragment
 
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val fragmentProfile = ProfileFragment()
     private val fragmentHome = HomeFragment()
+    private val fragmentGallery = GalleryFragment()
     private val fragmentCare = CareFragment()
+    private val fragmentCalendar = CalendarFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.background = null // hide abnormal layer in bottom nav
+
         switchFragment(fragmentHome)
 
         binding.bottomNavigationView.setOnNavigationItemSelectedListener {
@@ -35,12 +37,13 @@ class MainActivity : AppCompatActivity()  {
                 R.id.navigation_home -> switchFragment(fragmentHome)
                 R.id.navigation_profile -> switchFragment(fragmentProfile)
                 R.id.navigation_care -> startActivity(Intent(this, CareActivity::class.java))
-                R.id.navigation_gallery -> startActivity(Intent(this, GalleryActivity::class.java))
+                R.id.navigation_gallery -> switchFragment(fragmentGallery)
                 //R.id.navigation_calendar -> switchFragment(fragmentCalendar)
             }
             true
         }
-        }
+    }
+
 
     private fun switchFragment(fragment: Fragment) {
         supportFragmentManager
@@ -49,13 +52,13 @@ class MainActivity : AppCompatActivity()  {
             .commit()
     }
 
+
+
     fun routeToAuth() = startActivity(Intent(this, AuthActivity::class.java))
 
     override fun onBackPressed() {
         super.onBackPressed()
         finishAffinity()
     }
-
-
 
 }
